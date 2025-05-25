a87a18e966c29f8449bccf04e9123516694c14a4
Yukesh Katuwal <93563263+Ukeshgit@users.noreply.github.com>
2025-05-26 01:30:26 +0545

    sign in validation

--- lib/app/authentication/controller/sign_in_controller.dart
+++ lib/app/authentication/controller/sign_in_controller.dart
@@ -6,24 +6,52 @@ import 'package:learner/common/widgets/pop_up_messages.dart';
 class SignInController {
   WidgetRef ref;
   SignInController(this.ref);
-  void handlesignIn() {
+  Future<void> handlesignIn() async {
     var state = ref.read(signInNotifierProvider);
     String email = state.email;
     String password = state.password;
+
+    print("Your email is $email");
+    print("Your password is $password");
+
+    if (state.email.isEmpty) {
+      print("Your email is empty");
+      toastMessage(msg: "Your email is empty");
+      return;
+    } else if (state.password.isEmpty) {
+      print("Your password is empty");
+      toastMessage(msg: "Your password is empty");
+      return;
+    } else if (!email.contains('@') || !email.contains('.')) {
+      toastMessage(msg: 'Please enter a valid email address');
+      return;
+    }
+
     try {
-      FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+      final credentials = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      if (credentials.user == null) {
+        toastMessage(msg: "User doesn't exist.");
+      } else if (!credentials.user!.emailVerified) {
+        toastMessage(msg: "You must verify your email address first");
+      } else {
+        toastMessage(msg: "Login successful!");
+      }
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
+      if (e.code == 'invalid-email') {
+        toastMessage(msg: "Invalid email address.");
+      } else if (e.code == 'user-disabled') {
+        toastMessage(msg: "User has been disabled.");
+      } else if (e.code == 'user-not-found') {
+        toastMessage(msg: "No user found with this email.");
       } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
+        toastMessage(msg: "Incorrect password.");
+      } else {
+        toastMessage(msg: "Authentication failed. ${e.message}");
       }
     } catch (e) {
-      print("Exception is $e");
-      toastMessage(msg: e.toString());
+      toastMessage(msg: "An unexpected error occurred.");
     }
   }
 }
